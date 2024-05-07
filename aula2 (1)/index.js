0dbc0a5ff1d96f3e8fd2355a8c53f7c364f27bf3
Michaelsate <159037217+maicomsate@users.noreply.github.com>
2024-05-07 10:18:36 -0300

    Add files via upload

---
+++ aula2 (1)/index.js
@@ -0,0 +1,6 @@
+var nota1  = parseFloat(promp("Sua nota de Matematica"))
+var nota2  = parseFloat(promp("Sua nota de Português"))
+var nota3  = parseFloat(promp("Sua nota de História"))
+
+  var media = (nota1 + nota2 + nota3) /3
+ 
